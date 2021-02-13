604dfc083eec71679c42c99e6c23668d3acc7839
Nate <zmail1806@gmail.comm>
2021-02-13 10:50:38 -0600

    Stubed In Motor Configs

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -20,25 +23,38 @@ public class DriveTrain extends SubsystemBase {
   private final WPI_TalonFX m_frontRightMotor = new WPI_TalonFX(3);
   private final WPI_TalonFX m_backRightMotor = new WPI_TalonFX(4);
 
-
-  //Robots Drive Type: Differential Drive
+   //Robots Drive Type: Differential Drive
   private final DifferentialDrive m_drive = new DifferentialDrive(m_frontLeftMotor, m_frontRightMotor);
 
-   // Default threshold value from XboxController
-   double joyThreshold = 0.05;
+  // Default threshold value from XboxController
+  private static double joyThreshold = 0.05;
 
+  private  static final int kMaxNumberOfMotors = 4;
+  private WPI_TalonFX[] m_TalonFXs = new WPI_TalonFX[kMaxNumberOfMotors];
+  
    
   /** Creates a new DriveTrain. */
   public DriveTrain() {
+
+    // add motors to array
+    m_TalonFXs[0] = m_frontLeftMotor;
+    m_TalonFXs[1] = m_backLeftMotor;
+    m_TalonFXs[2] = m_frontRightMotor;
+    m_TalonFXs[3] = m_backRightMotor;
 
     //Set Masters and Followers
     m_backLeftMotor.follow(m_frontLeftMotor);
     m_backRightMotor.follow(m_frontRightMotor);
 
-    m_frontLeftMotor.configFactoryDefault();
-    m_backLeftMotor.configFactoryDefault();
-    m_frontRightMotor.configFactoryDefault();
-    m_backLeftMotor.configFactoryDefault();
+    int talonIndex = 0;
+
+    for(talonIndex = 0; talonIndex< kMaxNumberOfMotors; talonIndex++){
+        m_TalonFXs[talonIndex].configFactoryDefault();
+        m_TalonFXs[talonIndex].configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 20, 25, 1.0));
+        m_TalonFXs[talonIndex].configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 10, 15, 0.5));
+        m_TalonFXs[talonIndex].setNeutralMode(NeutralMode.Coast);
+      
+    }
 
    /* 
     m_frontLeftMotor.setInverted(true);
@@ -47,22 +63,18 @@ public class DriveTrain extends SubsystemBase {
     m_backRightMotor.setInverted(true);
    */
 
- /* m_frontLeftMotor.configPeakCurrentLimit(30); // don't activate current limit until current exceeds 30 A ...
-  talon.configPeakCurrentDuration(100); // ... for at least 100 ms
-  talon.configContinuousCurrentLimit(20); // once current-limiting is actived, hold at 20A
-  talon.enableCurrentLimit(true); */
-
-   // Drive Train Encoder Setup
-   m_frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, EncoderConstants.kPIDLoopIdx, EncoderConstants.kTimeoutMs);
-   m_frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, EncoderConstants.kPIDLoopIdx, EncoderConstants.kTimeoutMs);
+     // Drive Train Encoder Setup
+     m_frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, EncoderConstants.kPIDLoopIdx, EncoderConstants.kTimeoutMs);
+     m_frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, EncoderConstants.kPIDLoopIdx, EncoderConstants.kTimeoutMs);
   }
 
   @Override
   public void periodic() {
 
     // Pushing Drive Encoder Data to the SmartDashboard
-   SmartDashboard.putNumber(" FrontLeftSensorPosition", m_frontLeftMotor.getSelectedSensorPosition(Constants.EncoderConstants.kPIDLoopIdx));
-   SmartDashboard.putNumber(" BackLeftSensorPosition", m_backLeftMotor.getSelectedSensorPosition(Constants.EncoderConstants.kPIDLoopIdx));
+   SmartDashboard.putNumber(" FrontLeftSensorPosition", getLeftPosition());
+   SmartDashboard.putNumber(" FrontRightSensorPosition", getRightPosition());
+
   }
     // This method will be called once per scheduler run
   /**
@@ -76,24 +88,42 @@ public class DriveTrain extends SubsystemBase {
   
   //Arcade Drive from Drive via XboxController input   
   public void drive(double xSpeed, double ySpeed) {
-  
-    if(Math.abs(xSpeed) > joyThreshold || Math.abs(ySpeed) > joyThreshold) {
-      m_drive.arcadeDrive(xSpeed, ySpeed);
-
-      m_drive.arcadeDrive(ySpeed*-1.0, xSpeed*1.0);
-    }
-    else {
-      m_drive.arcadeDrive(0.0, 0.0);
-    }
+    m_drive.arcadeDrive(deadband(xSpeed), deadband(ySpeed), true);
 
   }
     
+ private double deadband( double value){
+   if (Math.abs(value)>joyThreshold){
+     return value;
+   }
+   else {
+    return 0.0;
+   }
+
+ }
  public void stopMotion(){
    m_frontLeftMotor.set(ControlMode.PercentOutput, 0.0);
    m_frontRightMotor.set(ControlMode.PercentOutput, 0.0);
 
   }
 
+  /**
+   * hello
+   * @return
+   * Native units for left Falcon drive encoder
+   */
+  public double getLeftPosition(){
+    return  m_frontLeftMotor.getSelectedSensorPosition(Constants.EncoderConstants.kPIDLoopIdx);
+  }
+
+  /**
+   * 
+   * @return
+   * NAtive units for right Falcon dive encoder
+   */
+  public double getRightPosition(){
+    return  m_frontRightMotor.getSelectedSensorPosition(Constants.EncoderConstants.kPIDLoopIdx);
+  }
 }
   
 
